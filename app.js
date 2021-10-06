20c2bed3759f210516315592069e629d1db30aa1
Valetín Feraudo <valentinferaudo@gmail.com>
2021-10-06 20:39:24 -0300

    Update exercise 5-d

--- app.js
+++ app.js
@@ -154,3 +154,12 @@ for (let i = 0; i < names.length; i++) {
         sentence = sentence + names[i];
 }
 window.alert(sentence);
+
+/* d - Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, 
+        es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9. 
+        Mostrar por la consola del navegador el array final (utilizar console.log).*/
+var emptyArray = [];
+for (let i = 0; i < 10; i++) {
+        emptyArray.push(i);
+}
+console.log(emptyArray)
