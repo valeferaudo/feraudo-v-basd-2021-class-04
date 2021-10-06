3c449c4042df0c45dd7e8e752a5d1c91cca941c0
Valetín Feraudo <valentinferaudo@gmail.com>
2021-10-06 16:58:55 -0300

    Update exercise 1-b

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ var num2 = 10;
 var total = num1 + num2;
 
 
+// b - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var _name = 'Valentín';
+var lastName = 'Feraudo';
+var nameLastName = _name + lastName;
+var nameLastNameSplit = `${_name} ${lastName}`
+console.log(nameLastName)
+console.log(nameLastNameSplit)
