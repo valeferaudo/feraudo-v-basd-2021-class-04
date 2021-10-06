d1eb1c160abd6591d6b6c1f6f28de415e2429961
Valetín Feraudo <valentinferaudo@gmail.com>
2021-10-06 20:29:11 -0300

    Update exercise 4-b

--- app.js
+++ app.js
@@ -100,3 +100,34 @@ if (x >= 0.5){
         window.alert('Lower than 0.5');
 }
 
+/* b - Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+        “Bebe” si la edad es menor a 2 años;
+        “Nino” si la edad es entre 2 y 12 años;
+        “Adolescente” entre 13 y 19 años;
+        “Joven” entre 20 y 30 años;
+        “Adulto” entre 31 y 60 años;
+        “Adulto mayor” entre 61 y 75 años;
+        “Anciano” si es mayor a 75 años. */
+
+var age = Math.floor(Math.random() * 100);
+if (age < 2){
+        window.alert('Bebe')
+}
+else if (age >= 2 && age <= 12){
+        window.alert('Niño')
+}
+else if (age >= 13 && age <= 19){
+        window.alert('Adolescente')
+}
+else if (age >= 20 && age <= 30){
+        window.alert('Joven')
+}
+else if (age >= 31 && age <= 60){
+        window.alert('Adulto')
+}
+else if (age >= 61 && age <= 75){
+        window.alert('Adulto mayor')
+}
+else if (age > 75){
+        window.alert('Anciano')
+}
