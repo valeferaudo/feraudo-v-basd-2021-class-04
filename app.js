03db7e9410cf23c97d47e0d34c47d18205767616
Valetín Feraudo <valentinferaudo@gmail.com>
2021-10-06 17:34:30 -0300

    Update exercise 3-b

--- app.js
+++ app.js
@@ -64,3 +64,8 @@ var newLongStringWithSpaces = longStringWithSpaces.substring(0,1).toUpperCase() 
         mostrar por consola los meses 5 y 11 (utilizar console.log).*/
 var monthArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 console.log(monthArray[4],monthArray[10]);
+
+// b - Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+
+monthArray.sort();
+console.log(monthArray)
