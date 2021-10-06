d6bdb38110e1b45d663a39cbb9256656797cfbf1
Valetín Feraudo <valentinferaudo@gmail.com>
2021-10-06 16:54:07 -0300

    Update exercise 1-a

---
+++ app.js
@@ -0,0 +1,7 @@
+// 1 - Variables y Operadores
+// a - Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var num1 = 5;
+var num2 = 10;
+var total = num1 + num2;
+
+
